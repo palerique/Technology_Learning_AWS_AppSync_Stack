3d1699b97ac75025789605b797837c2f9a213c30
Paulo Rodrigues <palerique@gmail.com>
2021-04-17 10:35:25 -0300

    ADD: typescript lambdas WIP

--- lambdas/lambda/deleteCommnent/src/index.ts
+++ lambdas/lambda/deleteCommnent/src/index.ts
@@ -1,33 +1,39 @@
-import {AppSyncResolverHandler} from 'aws-lambda';
-import AWS from "aws-sdk";
+import { AppSyncResolverHandler } from 'aws-lambda';
+import AWS from 'aws-sdk';
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-export const handler: AppSyncResolverHandler<DeleteGuestbookCommentInput, GuestbookComment> = async (event, context, callback) => {
+export const handler: AppSyncResolverHandler<DeleteGuestbookCommentInput, GuestbookComment> = async (
+  event,
+  context,
+  callback,
+) => {
   try {
-    const data = await dynamoDB.query({
-      TableName: process.env.TABLE_NAME,
-      IndexName: process.env.GSI_NAME,
-      KeyConditionExpression: "id = :id",
-      ExpressionAttributeValues: {
-        ":id": event.arguments.input.id
-      },
-    }).promise();
+    const data = await dynamoDB
+      .query({
+        TableName: process.env.TABLE_NAME,
+        IndexName: process.env.GSI_NAME,
+        KeyConditionExpression: 'id = :id',
+        ExpressionAttributeValues: {
+          ':id': event.arguments.input.id,
+        },
+      })
+      .promise();
     console.log(JSON.stringify(data));
     const [toBeDeleted] = data.Items;
-    let deleteParams = {
+    const deleteParams = {
       TableName: process.env.TABLE_NAME,
       Key: {
         guestbookId: toBeDeleted.guestbookId,
         createdDate: toBeDeleted.createdDate,
-      }
+      },
     };
 
     await dynamoDB.delete(deleteParams).promise();
-    callback(null, toBeDeleted)
+    callback(null, toBeDeleted);
   } catch (error) {
     const body = error.stack || JSON.stringify(error, null, 2);
-    console.error('ERROR!!!', body)
+    console.error('ERROR!!!', body);
     callback('ERROR!!!', body);
   }
 };
