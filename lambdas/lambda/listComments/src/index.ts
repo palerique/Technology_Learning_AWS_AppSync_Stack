62dafc1ac78d98222b93ae5c655459233b495154
Paulo Rodrigues <palerique@gmail.com>
2021-04-24 21:21:44 -0300

    adding dax

--- lambdas/lambda/listComments/src/index.ts
+++ lambdas/lambda/listComments/src/index.ts
@@ -1,13 +1,62 @@
 import { AppSyncResolverHandler } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
+import { ScanInput } from 'aws-sdk/clients/dynamodb';
 import { TableGuestbookCommentFilterInput } from 'generic-stuff/dist/types/TableGuestbookCommentFilterInput';
 import { GuestbookComment } from 'generic-stuff/dist/types/GuestbookComment';
-import { listComments } from 'generic-stuff/dist/services/listCommentsService';
+import { convertAttributeMapCollectionToCommentCollection } from 'generic-stuff/dist/util/Conversor';
+
+import * as AmazonDaxClient from 'amazon-dax-client';
 
 export const handler: AppSyncResolverHandler<TableGuestbookCommentFilterInput, { items: GuestbookComment[] }> = async (
   event,
 ) => {
-  console.log({ event });
-  const result = await listComments(event.arguments);
-  console.log({ result });
-  return result;
+  try {
+    // console.log({ commentFilterInput });
+    console.log({ env: process.env });
+    console.log(event);
+
+    const region = 'us-east-1';
+
+    AWS.config.update({
+      region,
+    });
+
+    // const ddbClient = new AWS.DynamoDB.DocumentClient();
+    console.log('creating dax');
+    const dax = new AmazonDaxClient({ endpoints: [process.env.DAX_URL || ''], region });
+    console.log('creating daxClient');
+    const daxClient = new AWS.DynamoDB.DocumentClient({ service: dax });
+
+    const filterExpression = undefined;
+    const expressionAttributeValues = undefined;
+
+    console.log({ daxClient, dax });
+
+    const params: ScanInput = {
+      TableName: process.env.TABLE_NAME || '',
+      Limit: 20, // TODO!!!
+      ExpressionAttributeValues: expressionAttributeValues,
+      FilterExpression: filterExpression,
+    };
+
+    console.log('calling scan');
+    const data = await daxClient.scan(params).promise();
+    console.log({ data });
+
+    let result: GuestbookComment[];
+
+    if (!data.Items || data.Items.length === 0) {
+      result = [];
+    } else {
+      result = convertAttributeMapCollectionToCommentCollection(data.Items);
+    }
+
+    console.log({ result });
+
+    return { items: result };
+  } catch (error) {
+    const body = error.stack || JSON.stringify(error, null, 2);
+    console.error('ERROR!!!', body);
+    throw error;
+  }
 };
