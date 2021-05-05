17ac81abdfd00fe40dc9b7362e4b3ed62d045aaf
Paulo Rodrigues <palerique@gmail.com>
2021-05-05 00:33:13 -0300

    add new results

--- gatling/src/gatling/scala/br/com/palerique/guestbook/AllOperationsSimulation.scala
+++ gatling/src/gatling/scala/br/com/palerique/guestbook/AllOperationsSimulation.scala
@@ -6,11 +6,12 @@ import io.gatling.http.Predef._
 import io.gatling.http.protocol.HttpProtocolBuilder
 
 import scala.concurrent.duration.DurationInt
+import scala.language.postfixOps
 
 class AllOperationsSimulation extends Simulation {
 
   val httpProtocol: HttpProtocolBuilder = http
-    .baseUrl("https://7jmo4bs3ezcf7fdpq2km6yeyqe.appsync-api.us-east-1.amazonaws.com")
+    .baseUrl("https://lbexwsny4vevrf37szamtlzjh4.appsync-api.us-east-1.amazonaws.com")
     .inferHtmlResources(BlackList(
       """.*\.js""",
       """.*\.css""",
@@ -30,17 +31,21 @@ class AllOperationsSimulation extends Simulation {
     .contentTypeHeader("application/json")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36")
 
-  val headers = Map("x-api-key" -> "da2-nob4sbxdxfgxvphknqyg4qnxdm")
+  val headers = Map("x-api-key" -> "da2-behai2leqfaprbdzcgjadije7m")
 
   val graphQl = "/graphql"
 
-  val scn: ScenarioBuilder = scenario("AllOperationsSimulation")
+  val allOperationsScn: ScenarioBuilder = scenario("AllOperationsSimulation")
     .exec(ListGuestbookComments.listGuestbookComments)
     .exec(GetGuestbookComment.getGuestbookComment)
     .exec(CreateGuestbookComment.createGuestbookComment)
     .exec(GetGuestbookComment.getGuestbookComment)
     .exec(DeleteGuestbookComment.deleteGuestbookComment)
     .exec(GetGuestbookComment.getDeletedGuestbookComment)
+
+  val readOperationsScn: ScenarioBuilder = scenario("ReadOperationsSimulation")
+    .exec(ListGuestbookComments.listGuestbookComments)
+    .exec(GetGuestbookComment.getGuestbookComment)
 
   object ListGuestbookComments {
     val listGuestbookComments: ChainBuilder = exec(http("listGuestbookComments")
@@ -109,7 +114,27 @@ class AllOperationsSimulation extends Simulation {
 
   setUp(
     //scn.inject(atOnceUsers(1))
-    scn.inject(atOnceUsers(100))
+    //    scn.inject(atOnceUsers(100))
+    //    scn.inject(rampUsers(3000).during(60.seconds))
     //scn.inject(rampUsers(2000).during(60.seconds))
+    //https://alexandreesl.com/2020/02/12/gatling-making-performance-tests-with-scala/
+    //    readOperationsScn.inject(constantUsersPerSec(250) during (5 minutes)),
+    //    allOperationsScn.inject(rampUsers(600) during (2 minutes))
+    //Others:
+    allOperationsScn.inject(
+      constantConcurrentUsers(10).during(20.seconds),
+      rampConcurrentUsers(10).to(20).during(20.seconds),
+      rampConcurrentUsers(20).to(30).during(20.seconds),
+      rampConcurrentUsers(30).to(40).during(20.seconds),
+      rampConcurrentUsers(40).to(50).during(20.seconds),
+      rampConcurrentUsers(50).to(60).during(20.seconds),
+      rampConcurrentUsers(60).to(70).during(20.seconds),
+      rampConcurrentUsers(70).to(80).during(20.seconds),
+      rampConcurrentUsers(80).to(90).during(20.seconds),
+      rampConcurrentUsers(90).to(100).during(20.seconds),
+      rampConcurrentUsers(100).to(200).during(20.seconds),
+      rampConcurrentUsers(200).to(300).during(20.seconds),
+      rampConcurrentUsers(300).to(500).during(20.seconds),
+    ),
   ).protocols(httpProtocol)
 }
